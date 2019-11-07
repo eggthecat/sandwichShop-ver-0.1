20d1c21df712ff34248a51289162ad57aef92c21
Student <example@example.com>
2019-11-07 15:41:53 -0800

    Add Sandwich.prototype.getPrice function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 // business interface
 
-function Sub (size, sides) {
+function Sandwich (size, sides) {
   this.size = size;
-  this.sides = sides
+  this.sides = sides;
+  // Automatically a number and value not determined by user
+  this.price = 0;
 }
+
+Sandwich.prototype.getPrice = function () {
+  this.sides.forEach(function(sides){
+    this.price =+1;
+  })
+  if(this.size===12) {
+    this.price+=5;
+  } else if(this.size===6){
+    this.price+=3;
+  }
+  return this.price;
+};
 
 // user interface
 $(document).ready(function(){
